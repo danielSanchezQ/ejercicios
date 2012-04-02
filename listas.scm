64951a3d72acef29f0160526e79edad83ba34adf
Daniel Sanchez <3danimanimal@gmail.com>
2012-04-02 13:55:12 +0200

    ejercicio 6 añadido, falta prueba con srfi1

--- listas.scm
+++ listas.scm
@@ -5,3 +5,12 @@
 ;;4
 (define (palindrome? list)
   (equal? list (reverse list)))
+
+;;6
+(define (rotate-rigth list n)
+  (let loop ((counter n)
+             (l list))
+  (if (= counter 0) 
+      (cdr l)
+      (cons (car (reverse l)) (loop (- n 1) 
+                                    (cdr l))))))
