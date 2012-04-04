783b39257d179ff0061b64b69755bade675df40c
Daniel Sanchez <3danimanimal@gmail.com>
2012-04-04 12:43:36 +0200

    all modules updated

--- listas.scm
+++ listas.scm
@@ -62,13 +62,6 @@
       (cons (list (take num l)) (list (loop (drop num l))))))))
 ;(grupo 2 '(a b c d e f g))
 ;;18
-(define (take-higher list)
-  (let loop ((l (cdr list))
-             (n (car list)))
-    (cond
-     ((null? l) n)
-     ((> n (car l)) (loop (cdr l) n))
-     ((< n (car l)) (loop (cdr l) (car l))))))
 (define (my-sort list)
   (let loop ((l list)
              (n (take-higher list)))
--- maths.scm
+++ maths.scm
@@ -1,36 +1,3 @@
-;;UTILITIES
-(define (list-operator operator list)
-  (let loop ((l list)
-             (result (cond
-                      ((eq? operator *) 1)
-                      ((eq? operator /) 1)
-                      (else
-                       0))))
-    (cond
-     ((null? l) result)
-     (else
-      (loop (cdr l) (operator result (car l)))))))
-
-(define (take-higher list)
-  (let loop ((l (cdr list))
-             (n (car list)))
-    (cond
-     ((null? l) n)
-     ((> n (car l)) (loop (cdr l) n))
-     ((< n (car l)) (loop (cdr l) (car l))))))
-
-(define (take-lower list)
-  (let loop ((l (cdr list))
-             (n (car list)))
-    (cond
-     ((null? l) n)
-     ((< n (car l)) (loop (cdr l) n))
-     ((> n (car l)) (loop (cdr l) (car l))))))
-;;(list-operator * '(1 2 3 4 5))
-;;(list-operator / '(1 2 3 4 5))
-;;(list-operator + '(1 2 3 4 5))
-;;(list-operator - '(1 2 3 4 5))
-
 
 ;;2 MODA
 (define (counter a list)
